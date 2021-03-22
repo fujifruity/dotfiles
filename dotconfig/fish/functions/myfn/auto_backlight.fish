c1dc3d219bfe2311030cd2f35965190472498681
fujifruity <fujifruity@gmail.com>
2021-03-22 20:15:15 +0900

    Fix crash when writing to /sys/class/backlight/intel_backlight

--- dotconfig/fish/functions/myfn/auto_backlight.fish
+++ dotconfig/fish/functions/myfn/auto_backlight.fish
@@ -2,16 +2,8 @@ function auto_backlight
     # brightness ∝ factor  
     set factor 60
 
-    set intel_bl /sys/class/backlight/intel_backlight
-    set max (cat $intel_bl'/max_brightness')
-    set now (cat $intel_bl'/brightness')
-    set path_to_now $intel_bl'/brightness'
     set mean (camera_pixels_mean)
-    set new_brightness (math $mean' * '$factor)
+    set level (math $mean' * '$factor)
+	brightnessctl set $level
 
-    echo brightness=$now new_brightness=$new_brightness camera_pixels_mean=$mean
-
-    echo $new_brightness >$path_to_now
-    # set -x ENABLE_SYSTEMD 1
-    # brightnessctl set $new_brightness
 end
